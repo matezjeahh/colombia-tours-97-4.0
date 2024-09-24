e63a8e8eb1e80fac28f6d56a3fd2431166fb697e
John Doe <matezjeahh1@gmail.com>
2024-09-24 22:36:09 +0200

    bvnm

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -18,15 +18,29 @@ export async function POST(request) {
     },
   });
 
-  const mailOptions = {
-    from: process.env.OUTLOOK_EMAIL,
-    to: process.env.OUTLOOK_EMAIL, // You can send to the same email or a different one
-    replyTo: email, // The email address of the person who filled out the form
-    subject: subject ? `Érdeklődés: ${subject}` : "Általános érdeklődés",
-    text: `${name}\n\nEmail: ${email}\n\n${message}`,
-  };
-
   try {
+    // Verify the connection configuration
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+        }
+      });
+    });
+
+    const mailOptions = {
+      from: process.env.OUTLOOK_EMAIL,
+      to: process.env.OUTLOOK_EMAIL, // You can send to the same email or a different one
+      replyTo: email, // The email address of the person who filled out the form
+      subject: subject ? `Érdeklődés: ${subject}` : "Általános érdeklődés",
+      text: `${name}\n\nEmail: ${email}\n\n${message}`,
+    };
+
+    // Send the email
     await new Promise((resolve, reject) => {
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
@@ -34,10 +48,11 @@ export async function POST(request) {
           reject(error);
         } else {
           console.log("Email sent:", info.response);
-          resolve();
+          resolve(info);
         }
       });
     });
+
     return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
   } catch (error) {
     console.error("Error sending email:", error);
