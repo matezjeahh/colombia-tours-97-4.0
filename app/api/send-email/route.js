78189cb8a5f8df7a483f7d1805a5b612d258ad23
John Doe <matezjeahh1@gmail.com>
2024-09-24 23:21:48 +0200

    dfg

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -1,3 +1,4 @@
+// app/api/send-email/route.js
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
@@ -7,8 +8,8 @@ export async function POST(request) {
   // Create a transporter using Outlook's SMTP settings
   const transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com", // Outlook's SMTP server
-    port: 465,
-    secure: true, // Use TLS
+    port: 587,
+    secure: false, // Use TLS
     auth: {
       user: process.env.OUTLOOK_EMAIL,
       pass: process.env.OUTLOOK_PASSWORD,
@@ -18,41 +19,16 @@ export async function POST(request) {
     },
   });
 
+  const mailOptions = {
+    from: process.env.OUTLOOK_EMAIL,
+    to: process.env.OUTLOOK_EMAIL, // You can send to the same email or a different one
+    replyTo: email, // The email address of the person who filled out the form
+    subject: subject ? `Érdeklődés: ${subject}` : "Általános érdeklődés",
+    text: `${name}\n\nEmail: ${email}\n\n${message}`,
+  };
+
   try {
-    // Verify the connection configuration
-    await new Promise((resolve, reject) => {
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log(error);
-          reject(error);
-        } else {
-          console.log("Server is ready to take our messages");
-          resolve(success);
-        }
-      });
-    });
-
-    const mailOptions = {
-      from: process.env.OUTLOOK_EMAIL,
-      to: process.env.OUTLOOK_EMAIL, // You can send to the same email or a different one
-      replyTo: email, // The email address of the person who filled out the form
-      subject: subject ? `Érdeklődés: ${subject}` : "Általános érdeklődés",
-      text: `${name}\n\nEmail: ${email}\n\n${message}`,
-    };
-
-    // Send the email
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error("Error sending email:", error);
-          reject(error);
-        } else {
-          console.log("Email sent:", info.response);
-          resolve(info);
-        }
-      });
-    });
-
+    await transporter.sendMail(mailOptions);
     return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
   } catch (error) {
     console.error("Error sending email:", error);
