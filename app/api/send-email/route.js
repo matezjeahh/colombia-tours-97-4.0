939d0df0ddaa220df36a1db2a1479e94a0d68b88
John Doe <matezjeahh1@gmail.com>
2024-09-25 16:37:45 +0200

    xcv

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -6,8 +6,8 @@ export async function POST(request) {
 
   const transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.OUTLOOK_EMAIL,
       pass: process.env.OUTLOOK_PASSWORD,
